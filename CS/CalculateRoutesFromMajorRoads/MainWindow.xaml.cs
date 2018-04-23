358e127cd782e503980f9d388a5dd8a3d1f9c76f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculateRoutesFromMajorRoads/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using DevExpress.Xpf.Map;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculateRoutesFromMajorRoads {
+    public partial class MainWindow : Window {
+        public GeoPoint location = new GeoPoint(40, -120);
+
+        public MainWindow() {
+            InitializeComponent();
+            this.DataContext = location;
+        }
+        
+        #region #RouteCalculated
+        private void routeDataProvider_RouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+            RouteCalculationResult result = e.CalculationResult;
+            
+            StringBuilder resultList = new StringBuilder();
+            resultList.Append(String.Format("Status: {0}\n", result.ResultCode));
+            resultList.Append(String.Format("Fault reason: {0}\n", result.FaultReason));
+            resultList.Append(ProcessIntermediatePoints(result.IntermediatePoints));
+            resultList.Append(ProcessRouteResults(result.RouteResults));
+            
+            tbResults.Text = resultList.ToString();
+        }
+        #endregion #RouteCalculated
+
+        #region #ProcessStartingPoints
+        string ProcessIntermediatePoints(List<RouteWaypoint> points) {
+            if (points == null) return "";
+
+            StringBuilder sb = new StringBuilder("Intermediate Points:\n"); 
+            sb.Append(String.Format("_________________________\n"));
+            for (int i = 0; i < points.Count; ++i)
+                    sb.Append(String.Format(
+                        "Intermediate point {0}: {1} ({2})\n", 
+                        i+1,
+                        points[i].Description, 
+                        points[i].Location
+                    ));
+            return sb.ToString();
+        }
+        #endregion #ProcessStartingPoints
+
+        #region #ProcessRouteResults
+        string ProcessRouteResults(List<BingRouteResult> results) {
+            if (results == null) return "";
+
+            StringBuilder sb = new StringBuilder("RouteResults:\n");
+            for (int i = 0; i < results.Count; i++) {
+                sb.Append(String.Format("_________________________\n"));
+                sb.Append(String.Format("Path {0}:\n", i+1));
+                sb.Append(String.Format("Distance: {0}\n", results[i].Distance));
+                sb.Append(String.Format("Time: {0}\n", results[i].Time));
+                sb.Append(ProcessLegs(results[i].Legs));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessRouteResults
+
+        #region #ProcessLegs
+        string ProcessLegs(List<BingRouteLeg> legs) {
+            if (legs == null) return "";
+            
+            StringBuilder sb = new StringBuilder("Legs:\n");    
+            for (int i = 0; i < legs.Count; i++) {
+                sb.Append(String.Format("\tLeg {0}:\n", i+1));
+                sb.Append(String.Format("\tStart: {0}\n", legs[i].StartPoint));
+                sb.Append(String.Format("\tEnd: {0}\n", legs[i].EndPoint));
+                sb.Append(String.Format("\tDistance: {0}\n", legs[i].Distance));
+                sb.Append(String.Format("\tTime: {0}\n", legs[i].Time));
+                sb.Append(ProcessItinerary(legs[i].Itinerary));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessLegs
+
+        #region #ProcessItinerary
+        string ProcessItinerary(List<BingItineraryItem> items) {
+            if (items == null) return "";
+            
+            StringBuilder sb = new StringBuilder("\tInternary Items:\n");
+            for (int i = 0; i < items.Count; i++) {
+                sb.Append(String.Format("\t\tItinerary {0}:\n", i+1));
+                sb.Append(String.Format("\t\tManeuver: {0}\n", items[i].Maneuver));
+                sb.Append(String.Format("\t\tLocation: {0}\n", items[i].Location));
+                sb.Append(String.Format("\t\tInstructions: {0}\n", items[i].ManeuverInstruction));
+                sb.Append(ProcessWarnings(items[i].Warnings));
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessItinerary
+
+        #region #ProcessWarnings
+        string ProcessWarnings(List<BingItineraryItemWarning> warnings) {
+            if (warnings == null) return "";
+
+            StringBuilder sb = new StringBuilder("\t\tWarnings:\n");
+            for (int i = 0; i < warnings.Count; i++) {
+                sb.Append(String.Format("\t\t\tWarning {0}:\n", i + 1));
+                sb.Append(String.Format("\t\t\tType: {0}\n", warnings[i].Type));
+                sb.Append(String.Format("\t\t\tText: {0}\n", warnings[i].Text));
+
+            }
+            return sb.ToString();
+        }
+        #endregion #ProcessWarnings
+
+        #region #CalculateRoutesFromMajorRoads
+        private void calculateRoutes_Click(object sender, RoutedEventArgs e) {
+            routeProvider.CalculateRoutesFromMajorRoads(new RouteWaypoint(tbDestination.Text, location));
+        }
+        #endregion #CalculateRoutesFromMajorRoads
+
+        private void ValidationError(object sender, ValidationErrorEventArgs e) {
+            if (e.Action == ValidationErrorEventAction.Added)
+                MessageBox.Show(e.Error.ErrorContent.ToString());
+        }
+    }
+
+    class RangeDoubleValidationRule : ValidationRule {
+        double min = Double.MinValue;
+        double max = Double.MaxValue;
+        public double Min {
+            get { return min; }
+            set {
+                if ((value != min) && (value <=max))
+                    min = value;
+            }
+        }
+        public double Max {
+            get { return max; }
+            set {
+                if ((value != max) && (value >= min))
+                    max = value;
+            }
+        }
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo) {
+            double d;
+            if (!Double.TryParse(value as string, out d))
+                return new ValidationResult(false, "Input value should be floating point number.");
+
+            if ((d < min) || (d > max))
+                return new ValidationResult(
+                    false, 
+                    String.Format(
+                        "Input value should be larger than or equals to {0} and less than or equals to {1}", 
+                        min, 
+                        max 
+                    )
+                );
+
+            return new ValidationResult(true, null);
+        }
+    }
+
+}
---
+++ VB/CalculateRoutesFromMajorRoads/MainWindow.xaml.vb
@@ -0,0 +1,175 @@
+﻿Imports DevExpress.Xpf.Map
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace CalculateRoutesFromMajorRoads
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public location As New GeoPoint(40, -120)
+
+        Public Sub New()
+            InitializeComponent()
+            Me.DataContext = location
+        End Sub
+
+        #Region "#RouteCalculated"
+        Private Sub routeDataProvider_RouteCalculated(ByVal sender As Object, ByVal e As BingRouteCalculatedEventArgs)
+            Dim result As RouteCalculationResult = e.CalculationResult
+
+            Dim resultList As New StringBuilder()
+            resultList.Append(String.Format("Status: {0}" & ControlChars.Lf, result.ResultCode))
+            resultList.Append(String.Format("Fault reason: {0}" & ControlChars.Lf, result.FaultReason))
+            resultList.Append(ProcessIntermediatePoints(result.IntermediatePoints))
+            resultList.Append(ProcessRouteResults(result.RouteResults))
+
+            tbResults.Text = resultList.ToString()
+        End Sub
+        #End Region ' #RouteCalculated
+
+        #Region "#ProcessStartingPoints"
+        Private Function ProcessIntermediatePoints(ByVal points As List(Of RouteWaypoint)) As String
+            If points Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("Intermediate Points:" & ControlChars.Lf)
+            sb.Append(String.Format("_________________________" & ControlChars.Lf))
+            For i As Integer = 0 To points.Count - 1
+                    sb.Append(String.Format("Intermediate point {0}: {1} ({2})" & ControlChars.Lf, i+1, points(i).Description, points(i).Location))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessStartingPoints
+
+        #Region "#ProcessRouteResults"
+        Private Function ProcessRouteResults(ByVal results As List(Of BingRouteResult)) As String
+            If results Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("RouteResults:" & ControlChars.Lf)
+            For i As Integer = 0 To results.Count - 1
+                sb.Append(String.Format("_________________________" & ControlChars.Lf))
+                sb.Append(String.Format("Path {0}:" & ControlChars.Lf, i+1))
+                sb.Append(String.Format("Distance: {0}" & ControlChars.Lf, results(i).Distance))
+                sb.Append(String.Format("Time: {0}" & ControlChars.Lf, results(i).Time))
+                sb.Append(ProcessLegs(results(i).Legs))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessRouteResults
+
+        #Region "#ProcessLegs"
+        Private Function ProcessLegs(ByVal legs As List(Of BingRouteLeg)) As String
+            If legs Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder("Legs:" & ControlChars.Lf)
+            For i As Integer = 0 To legs.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & "Leg {0}:" & ControlChars.Lf, i+1))
+                sb.Append(String.Format(ControlChars.Tab & "Start: {0}" & ControlChars.Lf, legs(i).StartPoint))
+                sb.Append(String.Format(ControlChars.Tab & "End: {0}" & ControlChars.Lf, legs(i).EndPoint))
+                sb.Append(String.Format(ControlChars.Tab & "Distance: {0}" & ControlChars.Lf, legs(i).Distance))
+                sb.Append(String.Format(ControlChars.Tab & "Time: {0}" & ControlChars.Lf, legs(i).Time))
+                sb.Append(ProcessItinerary(legs(i).Itinerary))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessLegs
+
+        #Region "#ProcessItinerary"
+        Private Function ProcessItinerary(ByVal items As List(Of BingItineraryItem)) As String
+            If items Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder(ControlChars.Tab & "Internary Items:" & ControlChars.Lf)
+            For i As Integer = 0 To items.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Itinerary {0}:" & ControlChars.Lf, i+1))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Maneuver: {0}" & ControlChars.Lf, items(i).Maneuver))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Location: {0}" & ControlChars.Lf, items(i).Location))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Instructions: {0}" & ControlChars.Lf, items(i).ManeuverInstruction))
+                sb.Append(ProcessWarnings(items(i).Warnings))
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessItinerary
+
+        #Region "#ProcessWarnings"
+        Private Function ProcessWarnings(ByVal warnings As List(Of BingItineraryItemWarning)) As String
+            If warnings Is Nothing Then
+                Return ""
+            End If
+
+            Dim sb As New StringBuilder(ControlChars.Tab & ControlChars.Tab & "Warnings:" & ControlChars.Lf)
+            For i As Integer = 0 To warnings.Count - 1
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Warning {0}:" & ControlChars.Lf, i + 1))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Type: {0}" & ControlChars.Lf, warnings(i).Type))
+                sb.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Text: {0}" & ControlChars.Lf, warnings(i).Text))
+
+            Next i
+            Return sb.ToString()
+        End Function
+        #End Region ' #ProcessWarnings
+
+        #Region "#CalculateRoutesFromMajorRoads"
+        Private Sub calculateRoutes_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            routeProvider.CalculateRoutesFromMajorRoads(New RouteWaypoint(tbDestination.Text, location))
+        End Sub
+        #End Region ' #CalculateRoutesFromMajorRoads
+
+        Private Sub ValidationError(ByVal sender As Object, ByVal e As ValidationErrorEventArgs)
+            If e.Action = ValidationErrorEventAction.Added Then
+                MessageBox.Show(e.Error.ErrorContent.ToString())
+            End If
+        End Sub
+    End Class
+
+    Friend Class RangeDoubleValidationRule
+        Inherits ValidationRule
+
+
+        Private min_Renamed As Double = Double.MinValue
+
+        Private max_Renamed As Double = Double.MaxValue
+        Public Property Min() As Double
+            Get
+                Return min_Renamed
+            End Get
+            Set(ByVal value As Double)
+                If (value <> min_Renamed) AndAlso (value <=max_Renamed) Then
+                    min_Renamed = value
+                End If
+            End Set
+        End Property
+        Public Property Max() As Double
+            Get
+                Return max_Renamed
+            End Get
+            Set(ByVal value As Double)
+                If (value <> max_Renamed) AndAlso (value >= min_Renamed) Then
+                    max_Renamed = value
+                End If
+            End Set
+        End Property
+
+        Public Overrides Function Validate(ByVal value As Object, ByVal cultureInfo As System.Globalization.CultureInfo) As ValidationResult
+            Dim d As Double = Nothing
+            If Not Double.TryParse(TryCast(value, String), d) Then
+                Return New ValidationResult(False, "Input value should be floating point number.")
+            End If
+
+            If (d < min_Renamed) OrElse (d > max_Renamed) Then
+                Return New ValidationResult(False, String.Format("Input value should be larger than or equals to {0} and less than or equals to {1}", min_Renamed, max_Renamed))
+            End If
+
+            Return New ValidationResult(True, Nothing)
+        End Function
+    End Class
+
+End Namespace

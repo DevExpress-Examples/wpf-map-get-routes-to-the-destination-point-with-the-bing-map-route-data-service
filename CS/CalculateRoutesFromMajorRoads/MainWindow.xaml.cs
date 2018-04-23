1e7ce8a173b11c86649a2338d647a11dbe3ea71e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculateRoutesFromMajorRoads/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using DevExpress.Xpf.Map;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CalculateRoutesFromMajorRoads {
+
+    public partial class MainWindow : Window {
+        
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        #region #calculateRoutesClick
+        double latitude;
+        double longitude;
+        string destination;
+        BingRouteOptions options = new BingRouteOptions();
+        
+        private void calculateRoutes_Click(object sender, RoutedEventArgs e) {
+            if (GetCalculateRoutesArguments()) {
+                CalculateMajorRoutes();
+            }
+        }
+
+        private bool GetCalculateRoutesArguments() {
+            latitude = String.IsNullOrEmpty(tbLatitude.Text) ? 0 : Double.Parse(tbLatitude.Text);
+            if ((latitude > 90) || (latitude < -90)) {
+                MessageBox.Show("Latitude must be less than or equal to 90 and greater than or equal to - 90. Please, correct the input value.");
+                return false;
+            }
+
+            longitude = String.IsNullOrEmpty(tbLongitude.Text) ? 0 : Double.Parse(tbLongitude.Text);
+            if ((longitude > 180) || (longitude < -180)) {
+                MessageBox.Show("Longitude must be less than or equal to 180 and greater than or equal to - 180. Please, correct the input value.");
+                return false;
+            }
+            
+            destination = tbDestination.Text;
+
+            if (cbMode.SelectedIndex == 0)
+                options.Mode = BingTravelMode.Driving;
+            else
+                options.Mode = BingTravelMode.Walking;
+
+            if (cbOptimize.SelectedIndex == 0)
+                options.RouteOptimization = BingRouteOptimization.MinimizeTime;
+            else
+                options.RouteOptimization = BingRouteOptimization.MinimizeDistance;
+
+            return true;
+        }
+
+        private void CalculateMajorRoutes() {
+            CalculateMajorRouteRequest(destination, new GeoPoint(latitude, longitude), options);
+        }
+
+        private void CalculateMajorRouteRequest(string destination, GeoPoint location, BingRouteOptions options) {
+            try {
+                #region #CalculateMajorRoutes
+                routeProvider.RouteOptions = options;
+                routeProvider.CalculateRoutesFromMajorRoads(new RouteWaypoint(destination, location), 2.0);
+                #endregion #CalculateMajorRoutes
+            }
+            catch (Exception ex) {
+                MessageBox.Show("An error occurs: " + ex.ToString());
+            }
+        }
+        #endregion #calculateRoutesClick
+
+        private void routeDataProvider_RouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+            mapControl1.CenterPoint = new GeoPoint(latitude, longitude);
+            mapControl1.ZoomLevel = 10;
+            RouteCalculationResult result = e.CalculationResult;
+            StringBuilder resultList = new StringBuilder("");
+            resultList.Append(String.Format("Status: {0}\n", result.ResultCode));
+            resultList.Append(String.Format("Fault reason: {0}\n", result.FaultReason));
+
+            if (result.StartingPoints != null) {
+                resultList.Append(String.Format("_________________________\n"));
+                int i = 1;
+                foreach (RouteWaypoint startingPoint in result.StartingPoints)
+                    resultList.Append(String.Format("Starting point {0}: {1} ({2})\n", i++,
+                        startingPoint.Description, startingPoint.Location));
+            }
+
+            if (result.RouteResults != null) {
+                int rnum = 1;
+                foreach (BingRouteResult routeResult in result.RouteResults) {
+                    resultList.Append(String.Format("_________________________\n"));
+                    resultList.Append(String.Format("Path {0}:\n", rnum++));
+                    resultList.Append(String.Format("Distance: {0}\n", routeResult.Distance));
+                    resultList.Append(String.Format("Time: {0}\n", routeResult.Time));
+
+                    MapPolyline path = new MapPolyline();
+                    path.Stroke = new SolidColorBrush(Colors.Red);
+                    path.StrokeStyle = new StrokeStyle() { Thickness = 2 };
+                    foreach (GeoPoint point in routeResult.RoutePath)
+                        path.Points.Add(point);
+
+                    if (routeResult.Legs != null) {
+                        int legNum = 1;
+                        foreach (BingRouteLeg leg in routeResult.Legs) {
+                            resultList.Append(String.Format("\tLeg {0}:\n", legNum++));
+                            resultList.Append(String.Format("\tStart: {0}\n", leg.StartPoint));
+                            resultList.Append(String.Format("\tEnd: {0}\n", leg.EndPoint));
+                            resultList.Append(String.Format("\tDistance: {0}\n", leg.Distance));
+                            resultList.Append(String.Format("\tTime: {0}\n", leg.Time));
+                            if (leg.Itinerary != null) {
+                                int itNum = 1;
+                                foreach (BingItineraryItem itineraryItem in leg.Itinerary) {
+                                    resultList.Append(String.Format("\t\tItinerary {0}:\n", itNum++));
+                                    resultList.Append(String.Format("\t\tManeuver: {0}\n", itineraryItem.Maneuver));
+                                    resultList.Append(String.Format("\t\tLocation: {0}\n", itineraryItem.Location));
+                                    resultList.Append(String.Format("\t\tInstructions: {0}\n", itineraryItem.ManeuverInstruction));
+                                    int warnNum = 1;
+                                    foreach (BingItineraryItemWarning warning in itineraryItem.Warnings) {
+                                        resultList.Append(String.Format("\t\t\tWarning {0}:\n", warnNum++));
+                                        resultList.Append(String.Format("\t\t\tType: {0}\n", warning.Type));
+                                        resultList.Append(String.Format("\t\t\tText: {0}\n", warning.Text));
+                                    }
+                                }
+                            }
+                            if (leg.StartPoint != null) {
+                                MapDot start = new MapDot();
+                                start.Size = 10;
+                                start.Location = leg.StartPoint;
+
+                            }
+                            if (leg.EndPoint != null) {
+                                MapDot end = new MapDot();
+                                end.Size = 15;
+                                end.Location = leg.EndPoint;
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            tbResults.Text = resultList.ToString();
+        }
+
+    }
+}
+
---
+++ VB/CalculateRoutesFromMajorRoads/MainWindow.xaml.vb
@@ -0,0 +1,156 @@
+﻿Imports DevExpress.Xpf.Map
+Imports System
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Media
+
+Namespace CalculateRoutesFromMajorRoads
+
+    Partial Public Class MainWindow
+        Inherits Window
+
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        #Region "#calculateRoutesClick"
+        Private latitude As Double
+        Private longitude As Double
+        Private destination As String
+        Private options As New BingRouteOptions()
+
+        Private Sub calculateRoutes_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            If GetCalculateRoutesArguments() Then
+                CalculateMajorRoutes()
+            End If
+        End Sub
+
+        Private Function GetCalculateRoutesArguments() As Boolean
+            latitude = If(String.IsNullOrEmpty(tbLatitude.Text), 0, Double.Parse(tbLatitude.Text))
+            If (latitude > 90) OrElse (latitude < -90) Then
+                MessageBox.Show("Latitude must be less than or equal to 90 and greater than or equal to - 90. Please, correct the input value.")
+                Return False
+            End If
+
+            longitude = If(String.IsNullOrEmpty(tbLongitude.Text), 0, Double.Parse(tbLongitude.Text))
+            If (longitude > 180) OrElse (longitude < -180) Then
+                MessageBox.Show("Longitude must be less than or equal to 180 and greater than or equal to - 180. Please, correct the input value.")
+                Return False
+            End If
+
+            destination = tbDestination.Text
+
+            If cbMode.SelectedIndex = 0 Then
+                options.Mode = BingTravelMode.Driving
+            Else
+                options.Mode = BingTravelMode.Walking
+            End If
+
+            If cbOptimize.SelectedIndex = 0 Then
+                options.RouteOptimization = BingRouteOptimization.MinimizeTime
+            Else
+                options.RouteOptimization = BingRouteOptimization.MinimizeDistance
+            End If
+
+            Return True
+        End Function
+
+        Private Sub CalculateMajorRoutes()
+            CalculateMajorRouteRequest(destination, New GeoPoint(latitude, longitude), options)
+        End Sub
+
+        Private Sub CalculateMajorRouteRequest(ByVal destination As String, ByVal location As GeoPoint, ByVal options As BingRouteOptions)
+            Try
+'                #Region "#CalculateMajorRoutes"
+                routeProvider.RouteOptions = options
+                routeProvider.CalculateRoutesFromMajorRoads(New RouteWaypoint(destination, location), 2.0)
+'                #End Region ' #CalculateMajorRoutes
+            Catch ex As Exception
+                MessageBox.Show("An error occurs: " & ex.ToString())
+            End Try
+        End Sub
+        #End Region ' #calculateRoutesClick
+
+        Private Sub routeDataProvider_RouteCalculated(ByVal sender As Object, ByVal e As BingRouteCalculatedEventArgs)
+            mapControl1.CenterPoint = New GeoPoint(latitude, longitude)
+            mapControl1.ZoomLevel = 10
+            Dim result As RouteCalculationResult = e.CalculationResult
+            Dim resultList As New StringBuilder("")
+            resultList.Append(String.Format("Status: {0}" & ControlChars.Lf, result.ResultCode))
+            resultList.Append(String.Format("Fault reason: {0}" & ControlChars.Lf, result.FaultReason))
+
+            If result.StartingPoints IsNot Nothing Then
+                resultList.Append(String.Format("_________________________" & ControlChars.Lf))
+                Dim i As Integer = 1
+                For Each startingPoint As RouteWaypoint In result.StartingPoints
+                    resultList.Append(String.Format("Starting point {0}: {1} ({2})" & ControlChars.Lf, i, startingPoint.Description, startingPoint.Location))
+                Next startingPoint
+                    i += 1
+            End If
+
+            If result.RouteResults IsNot Nothing Then
+                Dim rnum As Integer = 1
+                For Each routeResult As BingRouteResult In result.RouteResults
+                    resultList.Append(String.Format("_________________________" & ControlChars.Lf))
+                    resultList.Append(String.Format("Path {0}:" & ControlChars.Lf, rnum))
+                    rnum += 1
+                    resultList.Append(String.Format("Distance: {0}" & ControlChars.Lf, routeResult.Distance))
+                    resultList.Append(String.Format("Time: {0}" & ControlChars.Lf, routeResult.Time))
+
+                    Dim path As New MapPolyline()
+                    path.Stroke = New SolidColorBrush(Colors.Red)
+                    path.StrokeStyle = New StrokeStyle() With {.Thickness = 2}
+                    For Each point As GeoPoint In routeResult.RoutePath
+                        path.Points.Add(point)
+                    Next point
+
+                    If routeResult.Legs IsNot Nothing Then
+                        Dim legNum As Integer = 1
+                        For Each leg As BingRouteLeg In routeResult.Legs
+                            resultList.Append(String.Format(ControlChars.Tab & "Leg {0}:" & ControlChars.Lf, legNum))
+                            legNum += 1
+                            resultList.Append(String.Format(ControlChars.Tab & "Start: {0}" & ControlChars.Lf, leg.StartPoint))
+                            resultList.Append(String.Format(ControlChars.Tab & "End: {0}" & ControlChars.Lf, leg.EndPoint))
+                            resultList.Append(String.Format(ControlChars.Tab & "Distance: {0}" & ControlChars.Lf, leg.Distance))
+                            resultList.Append(String.Format(ControlChars.Tab & "Time: {0}" & ControlChars.Lf, leg.Time))
+                            If leg.Itinerary IsNot Nothing Then
+                                Dim itNum As Integer = 1
+                                For Each itineraryItem As BingItineraryItem In leg.Itinerary
+                                    resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Itinerary {0}:" & ControlChars.Lf, itNum))
+                                    itNum += 1
+                                    resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Maneuver: {0}" & ControlChars.Lf, itineraryItem.Maneuver))
+                                    resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Location: {0}" & ControlChars.Lf, itineraryItem.Location))
+                                    resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & "Instructions: {0}" & ControlChars.Lf, itineraryItem.ManeuverInstruction))
+                                    Dim warnNum As Integer = 1
+                                    For Each warning As BingItineraryItemWarning In itineraryItem.Warnings
+                                        resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Warning {0}:" & ControlChars.Lf, warnNum))
+                                        warnNum += 1
+                                        resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Type: {0}" & ControlChars.Lf, warning.Type))
+                                        resultList.Append(String.Format(ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Text: {0}" & ControlChars.Lf, warning.Text))
+                                    Next warning
+                                Next itineraryItem
+                            End If
+                            If leg.StartPoint <> Nothing Then
+                                Dim start As New MapDot()
+                                start.Size = 10
+                                start.Location = leg.StartPoint
+
+                            End If
+                            If leg.EndPoint <> Nothing Then
+                                Dim [end] As New MapDot()
+                                [end].Size = 15
+                                [end].Location = leg.EndPoint
+
+                            End If
+                        Next leg
+                    End If
+                Next routeResult
+            End If
+
+            tbResults.Text = resultList.ToString()
+        End Sub
+
+    End Class
+End Namespace
+
